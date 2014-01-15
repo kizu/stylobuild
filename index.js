d5e957652d4a849be63a8f65280e80ab3878f895
Roman Komarov <kizmarh@ya.ru>
2014-01-15 13:44:33 +0400

    Added ability to pass options to the used plugins, fixes #2

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ var autoprefixer = require('autoprefixer');
 var csso = require('csso');
 var pixrem = require('pixrem');
 
-module.exports = function() {
+module.exports = function(options) {
     return function() {
         this.on('end', function(err, css) {
             var result = css;
-            result = autoprefixer.compile(result);
+            options = options || {};
+
+            // Do not use the fallback options
+            if (options.filename && options.Evaluator) {
+                options = {};
+            }
+
+            // Using the autoprefixer
+            var autoprefixer_browsers = (options.autoprefixer && options.autoprefixer.browsers) || [];
+            var autoprefixer_options = options.autoprefixer || {};
+            if (autoprefixer_browsers.length) {
+                autoprefixer_browsers = autoprefixer_browsers.split(/,\s*/);
+            }
+            result = autoprefixer.apply(true, autoprefixer_browsers).process(result, autoprefixer_options).css;
+
+            // Using the CSSO
             result = csso.justDoIt(result);
-            result = pixrem(result, '10px');
+
+            // Using the pixrem
+            var pixrem_rootvalue = (options.pixrem && options.pixrem.rootvalue) || '10px';
+            var pixrem_options = options.pixrem || {};
+            result = pixrem(result, pixrem_rootvalue, pixrem_options);
             return result;
         });
     };
