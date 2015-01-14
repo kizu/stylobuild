2a597dee31e74d686a0d06fceb82fc46ae0f3cd7
Artem Sapegin <artem@sapegin.ru>
2015-01-14 21:01:49 +0300

    clean-css 3.0.4.

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ var stylobuild_csso = function(stylobuild) {
 var stylobuild_cleancss = function(stylobuild) {
     if (!stylobuild.sourcemap && stylobuild.options.cleancss !== false && stylobuild.options.minifier === 'cleancss') {
         var cleancss_options = stylobuild.options.cleancss || {};
-        stylobuild.css = new cleancss(cleancss_options).minify(stylobuild.css);
+        stylobuild.css = new cleancss(cleancss_options).minify(stylobuild.css).styles;
     }
 }
 
